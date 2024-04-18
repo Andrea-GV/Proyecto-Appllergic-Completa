7dc6ca46cf2bbd98ae6c0ae2ec3dfcd9532e77dd
Andrea-GV <andrea.garcia.valverde@bootcamp-upgrade.com>
2024-04-18 18:45:27 +0200

    una mierda de contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import RegisterContact from './pages/login/registerContact/RegisterContact'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<RegisterContact></RegisterContact>}>
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }

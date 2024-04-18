37a6e46ebca7acef2bfd030f4c0f627d5209634a
anabh05 <anabh05@gmail.com>
2024-04-18 19:04:33 +0200

    prueba

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import './index.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Home from './pages/home/Home.jsx';
 

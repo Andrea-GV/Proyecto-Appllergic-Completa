ffa2d1e3e1a00f60eb6d5a53f26a2fa1af27121c
freelivecode <mariogilruiz66@gmail.com>
2024-04-19 10:47:05 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import RegisterContact from './pages/login/registerContact/RegisterContact'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import RegisterContact from './pages/login/registerContact/RegisterContact';
 
-import './App.css'
-import LoginPage from './pages/login/loginPage/LoginPage'
+import './App.css';
+import LoginPage from './pages/login/loginPage/LoginPage';
+import Home from './pages/home/Home';
+import { useEffect } from 'react';
+import Scanner from './pages/scanner/Scanner';
+import Splash from './pages/splash/Splash';
+import RegisterFinalPage from './pages/login/registerFinalPage/RegisterFinalPage';
+import ResgiterPage from './pages/login/registerPage/ResgiterPage';
+import Profile from './pages/user/profile/Profile';
 
+//const navigate = useNavigate()
 
 function App() {
+
+
+  useEffect(() => {
+
+    //navigate("/OnBoarding")
+  }, [])
 
   return (
 
@@ -14,14 +28,54 @@ function App() {
       <BrowserRouter>
 
         <Routes>
-          <Route path="/" element={<RegisterContact></RegisterContact>}>
 
-          </Route>
+          <Route
+            path="/"
+            element={<Home></Home>} />
+
+          <Route
+            path='/login'
+            element={<LoginPage></LoginPage>} />
+
+          <Route
+            path='register'
+            element={<ResgiterPage></ResgiterPage>} />
+
+          <Route
+            path='/register/registerContact'
+            element={<RegisterContact></RegisterContact>} />
+
+          <Route
+            path='register/final'
+            element={<RegisterFinalPage></RegisterFinalPage>} />
+
+          <Route
+            path='/user/profile'
+            element={<Profile></Profile>} />
+
+          <Route
+            path='/scanner'
+            element={<Scanner></Scanner>} />
+
+          {/* <Route 
+          path='/scanner/result' 
+          element={ } /> */}
+
+          <Route
+            path='/splash'
+            element={<Splash></Splash>} />
+
+
+
+          {/* <Route path='/allergy' element={ } /> */}
+
+
+
         </Routes>
 
       </BrowserRouter>
 
-      <LoginPage></LoginPage>
+
 
     </>
 

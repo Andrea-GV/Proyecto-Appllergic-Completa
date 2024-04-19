dbafbc9ee2e176ddfd6f5ad00d7733ed5458efbf
Laureana <laureanagg94@gmail.com>
2024-04-19 21:09:51 +0200

    actualizacion de rutas slides y rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,72 @@
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import RegisterContact from "./pages/login/registerContact/RegisterContact";
 
-
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
-import RegisterContact from './pages/login/registerContact/RegisterContact';
-
-
-import './App.css';
-import LoginPage from './pages/login/loginPage/LoginPage';
-import Home from './pages/home/Home';
-import { useEffect } from 'react';
-import Scanner from './pages/scanner/Scanner';
-import Splash from './pages/splash/Splash';
-import RegisterFinalPage from './pages/login/registerFinalPage/RegisterFinalPage';
-import ResgiterPage from './pages/login/registerPage/ResgiterPage';
-import Profile from './pages/user/profile/Profile';
-
-
-
-//import OnBoarding from './pages/onBoarding/OnBoarding'
+import "./App.css";
+import LoginPage from "./pages/login/loginPage/LoginPage";
+import Home from "./pages/home/Home";
+import { useEffect } from "react";
+import Scanner from "./pages/scanner/Scanner";
+import Splash from "./pages/splash/Splash";
+import RegisterFinalPage from "./pages/login/registerFinalPage/RegisterFinalPage";
+import ResgiterPage from "./pages/login/registerPage/ResgiterPage";
+import Profile from "./pages/user/profile/Profile";
+import AllergyPage from "./pages/login/allergyPage/AllergyPage";
+import Valoracion from "./components/valoracion/Valoracion";
+import SlidesOnBoard from "./components/slides/SlidesOnBoard/SlidesOnBoard"
+import OnBoarding from './pages/onBoarding/OnBoarding'
 
 //const navigate = useNavigate()
 
-
 function App() {
-
-
-
   useEffect(() => {
-
     //navigate("/OnBoarding")
-  }, [])
-
+  }, []);
 
   return (
     <>
-      {/* <OnBoarding></OnBoarding> */}
+      { <OnBoarding></OnBoarding> }
 
       <BrowserRouter>
         <Routes>
           <Route path="/allergy" element={<AllergyPage></AllergyPage>}></Route>
 
-      
+          <Route path="/" element={<Home></Home>} />
 
+          <Route path="/login" element={<LoginPage></LoginPage>} />
 
-          <Route
-            path="/"
-            element={<Home></Home>} />
-
-          <Route
-            path='/login'
-            element={<LoginPage></LoginPage>} />
+          <Route path="/register" element={<ResgiterPage></ResgiterPage>} />
 
           <Route
-            path='/register'
-            element={<ResgiterPage></ResgiterPage>} />
+            path="/register/registerContact"
+            element={<RegisterContact></RegisterContact>}
+          />
 
           <Route
-            path='/register/registerContact'
-            element={<RegisterContact></RegisterContact>} />
+            path="/register/final"
+            element={<RegisterFinalPage></RegisterFinalPage>}
+          />
 
-          <Route
-            path='/register/final'
-            element={<RegisterFinalPage></RegisterFinalPage>} />
+          <Route path="/user/profile" element={<Profile></Profile>} />
 
-          <Route
-            path='/user/profile'
-            element={<Profile></Profile>} />
-
-          <Route
-            path='/scanner'
-            element={<Scanner></Scanner>} />
+          <Route path="/scanner" element={<Scanner></Scanner>} />
 
           {/* <Route 
           path='/scanner/result' 
           element={ } /> */}
 
-          <Route
-            path='/splash'
-            element={<Splash></Splash>} />
+          <Route path="/splash" element={<Splash></Splash>} />
 
           {/* <Route path='/allergy' element={ } /> */}
+
+          <Route path="/valoracion" element={<Valoracion></Valoracion>} />
+
+
+          <Route path="/slides" element={<SlidesOnBoard></SlidesOnBoard>}/>
 
 
 
         </Routes>
       </BrowserRouter>
-
-
-
-
     </>
   );
 }
